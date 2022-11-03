0e3efad43f2411eb9703195aa62d96b24ec7e9b2
Mikaelwendin <mikael.wendin83@gmail.com>
2022-11-03 09:46:14 +0100

    ändrade lite variabelnamn

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 class Todos {
-  constructor(what,isDone) {
+  constructor(what, isDone) {
     this.isDone = isDone || false;
     this.what = what;
   }
@@ -7,14 +7,14 @@ class Todos {
 let doneContainer = document.getElementById("myList");
 let notDoneContainer = document.getElementById("mySecondList");
 let objList = [
-  new Todos("första saken",  false),
-  new Todos( "Andra saken",  false),
-  new Todos( "tredje saken",  false),
-  new Todos( "fjärde saken",  false),
-  new Todos( "femte saken",  false),
-  new Todos( "sjätte saken",  false),
-  new Todos( "sjunde saken",  false),
-  new Todos( "åttonde saken",  false),
+  new Todos("första saken", false),
+  new Todos("Andra saken", false),
+  new Todos("tredje saken", false),
+  new Todos("fjärde saken", false),
+  new Todos("femte saken", false),
+  new Todos("sjätte saken", false),
+  new Todos("sjunde saken", false),
+  new Todos("åttonde saken", false),
 ];
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -29,19 +29,15 @@ document.getElementById("btn--delete").addEventListener("click", () => {
 });
 
 document.getElementById("btn").addEventListener("click", () => {
-  if (document.getElementById("first").value.length !== 0)
-  {
-  createToDo(
-    document.getElementById("first").value);
+  if (document.getElementById("first").value.length !== 0) {
+    createToDo(document.getElementById("first").value);
     document.getElementById("first").value = "";
-  }
-  else {
+  } else {
     window.alert("Ya'll need to type somethin'");
   }
 });
 
 function addToDo(obj) {
-  //sessionStorage.setItem(obj.id, JSON.stringify(obj));
   let toDoItems = document.createElement("li");
   if (obj.isDone === false) {
     doneContainer.appendChild(toDoItems);
@@ -57,7 +53,6 @@ function addToDo(obj) {
 }
 
 function createToDo(userWhat, isDone) {
-  
   let newToDo = new Todos(userWhat, isDone);
   addToDo(newToDo);
   objList.push(newToDo);
@@ -67,12 +62,10 @@ function handleClick(obj, element) {
     obj.isDone = true;
     element.innerText = "Done!";
     element.classList.toggle("disable");
-    setTimeout(() =>  {
+    setTimeout(() => {
       element.remove();
-      
     }, 1000);
     addToDo(obj);
-    
   } else {
     obj.isDone = false;
     element.remove();
@@ -80,22 +73,20 @@ function handleClick(obj, element) {
   }
 }
 
-function sortList(oList) {
+function sortList(listToSort) {
   let element = document.getElementById("myList");
-  let sortedList = oList;
+  let sortedList = listToSort;
   if (sortedList) {
-    sortedList.sort((a, b) =>
-      a.what > b.what ? 1 : a.what === b.what -1
-    );
+    sortedList.sort((a, b) => (a.what > b.what ? 1 : a.what === b.what - 1));
     element.innerHTML = "";
     for (let i = 0; i < sortedList.length; i++) {
-      oList[i] = sortedList[i];
-      if (oList[i].isDone === false) {
-        addToDo(oList[i]);
+      listToSort[i] = sortedList[i];
+      if (listToSort[i].isDone === false) {
+        addToDo(listToSort[i]);
       }
     }
   } else {
-    window.alert("lol");
+    window.alert("Nåt gick fel");
   }
 }
 function deleteFromList(listToSearch, target, element) {
